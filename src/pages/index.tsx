57c4654a6f4daf41c135a28eed6e82cfe56878d3
naoto hamada <hamada@vis-its.com>
2020-03-22 23:53:42 +0900

    Remove page for sendgrid

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,6 @@ const MarunouchiLunch = lazy(() =>
 );
 const Login = lazy(() => import(/* webpackChunkName: 'login' */ './Login'));
 const Top = lazy(() => import(/* webpackChunkName: 'top' */ './Top'));
-const Sendgrid = lazy(() =>
-  import(/* webpackChunkName: 'sendgrid' */ './Sendgrid'),
-);
 
 export default function RootRouter() {
   return (
@@ -16,7 +13,6 @@ export default function RootRouter() {
       <Switch>
         <Route path="/marunouchi-lunch" exact component={MarunouchiLunch} />
         <Route path="/login" exact component={Login} />
-        <Route path="/sendgrid" exact component={Sendgrid} />
         <Route component={Top} />
       </Switch>
     </Suspense>
