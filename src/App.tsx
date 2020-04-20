45df5b35b8d8d311fc2df827d32c2f8d63a83df5
naoto hamada <hamada@vis-its.com>
2020-04-20 17:31:51 +0900

    Add terms of service

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import RootRouter from 'pages';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider, ColorModeProvider } from '@chakra-ui/core';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <RootRouter />
+      <ThemeProvider>
+        <ColorModeProvider>
+          <RootRouter />
+        </ColorModeProvider>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
