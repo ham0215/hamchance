b9926f66697ab9625c4f8d21c321c87d4c2044b4
naoto hamada <hamada@vis-its.com>
2020-04-21 23:19:23 +0900

    Fix

--- src/pages/MarunouchiLunch/index.tsx
+++ src/pages/MarunouchiLunch/index.tsx
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import firebase from 'components/Firebase';
+import { User } from 'firebase/app';
 
 export default function MarunouchiLunch() {
-  return <div>marunouchi lunch</div>;
+  const [currentUser, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => {
+      setUser(user);
+    });
+  });
+
+  if (currentUser) {
+    return (
+      <div>
+        <div>Logged in</div>
+        <button type="button" onClick={() => firebase.auth().signOut()}>
+          signOut
+        </button>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <div>Not logged in</div>
+      <Link to="/login">Login</Link>
+    </div>
+  );
 }
--- src/pages/Top/index.tsx
+++ src/pages/Top/index.tsx
@@ -1,31 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import firebase from 'components/Firebase';
-import { User } from 'firebase/app';
+import React from 'react';
 
 export default function Top() {
-  const [currentUser, setUser] = useState<User | null>(null);
-
-  useEffect(() => {
-    firebase.auth().onAuthStateChanged(user => {
-      setUser(user);
-    });
-  });
-
-  if (currentUser) {
-    return (
-      <div>
-        <div>Logged in</div>
-        <button type="button" onClick={() => firebase.auth().signOut()}>
-          signOut
-        </button>
-      </div>
-    );
-  }
   return (
     <div>
-      <div>Not logged in</div>
-      <Link to="/login">Login</Link>
+      <div>TOP</div>
     </div>
   );
 }
