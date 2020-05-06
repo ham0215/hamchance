c2bf937846b96ef6ae1364196d1007fa0718ea42
naoto hamada <hamada@vis-its.com>
2020-05-07 01:40:39 +0900

    Add twitter

---
+++ src/components/Twitter.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import TwitterIcon from 'images/twitter.svg';
+
+export default function Twitter() {
+  return (
+    <div>
+      <img src={TwitterIcon} alt="twitter" width="50px" />
+      <span>@ham</span>
+    </div>
+  );
+}
--- src/pages/Top/Profile.tsx
+++ src/pages/Top/Profile.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import SectionHeading from 'components/SectionHeading';
 import SectionText from 'components/SectionText';
+import Twitter from 'components/Twitter';
 
 const Section = styled.section`
   padding: 80px 0;
@@ -11,6 +12,7 @@ export default function Profile() {
   return (
     <Section id="profile">
       <SectionHeading>PROFILE</SectionHeading>
+      <Twitter />
       <SectionText>開発中...!!</SectionText>
     </Section>
   );
