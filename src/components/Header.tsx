115ed2cd7947c570cb951eabfb0206b8e5e5dcde
naoto hamada <hamada@vis-its.com>
2020-05-17 14:04:30 +0900

    Refactoring header menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,19 +3,13 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
 import Link from '@material-ui/core/Link';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import Button from '@material-ui/core/Button';
 import firebase from 'components/Firebase';
 import { User } from 'firebase/app';
+import HeaderMenu from 'components/HeaderMenu';
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
   menuButton: {
     marginRight: theme.spacing(2),
   },
@@ -28,21 +22,12 @@ const useStyles = makeStyles(theme => ({
 export default function Header() {
   const classes = useStyles();
   const [currentUser, setUser] = useState<User | null>(null);
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged(user => {
       setUser(user);
     });
   });
-
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
 
   let loginButton;
   if (currentUser) {
@@ -62,26 +47,7 @@ export default function Header() {
   return (
     <AppBar position="static">
       <Toolbar>
-        <IconButton
-          edge="start"
-          className={classes.menuButton}
-          color="inherit"
-          aria-label="menu"
-          onClick={handleClick}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Menu
-          id="simple-menu"
-          anchorEl={anchorEl}
-          keepMounted
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
-        >
-          <MenuItem onClick={handleClose}>Profile</MenuItem>
-          <MenuItem onClick={handleClose}>My account</MenuItem>
-          <MenuItem onClick={handleClose}>Logout</MenuItem>
-        </Menu>
+        <HeaderMenu />
         <Typography variant="h6" className={classes.title}>
           <Link href="/" color="inherit" underline="none">
             Hamchance.com
