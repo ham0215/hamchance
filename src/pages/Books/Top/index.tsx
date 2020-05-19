fb8027c1b79b355ab38725bdb02ce53746aedfc8
naoto hamada <hamada@vis-its.com>
2020-05-19 17:49:08 +0900

    Add books

--- src/pages/Books/Top/index.tsx
+++ src/pages/Books/Top/index.tsx
@@ -1,48 +1,67 @@
 import React from 'react';
 import styled from 'styled-components/macro';
+import SectionHeading from 'components/SectionHeading';
 
 const Contents = styled.div`
-  background: #151515 url('/images/tundoku.png') no-repeat center;
-  background-size: contain;
-  color: #fff;
   text-align: center;
+`;
+
+const Header = styled.div`
+  background: #151515 url('/images/tundoku.png') no-repeat center;
+  background-size: 200px;
+  color: #fff;
+  height: 250px;
+  padding: 50px 0;
+`;
+
+const Books = styled.div`
+  margin: 20px 0;
+`;
+
+const Book = styled.li`
+  position: relative;
+  width: 240px;
+  margin: 1px auto;
+  padding: 15px;
+  font-size: 16px;
+  line-height: 1.3;
+  text-align: center;
+  letter-spacing: 0.05em;
+  border-radius: 8px;
+  background-image: linear-gradient(
+    0deg,
+    #fff 25%,
+    #8e8e8e 25%,
+    #8e8e8e 50%,
+    #fff 50%,
+    #fff 75%,
+    #8e8e8e 75%,
+    #8e8e8e
+  );
+  background-size: 4px 4px;
+  list-style-type: none;
+  border-top: 3px solid green;
+  border-bottom: 3px solid green;
+  border-left: 3px solid green;
+  border-top-right-radius: 0;
+  border-bottom-right-radius: 0;
 `;
 
 export default function Top() {
   return (
     <Contents>
-      Books
-      <br />
-      Books
-      <br />
-      Books
-      <br />
-      Books
-      <br />
-      Books
-      <br />
-      Books
-      <br />
-      Books
-      <br />
-      Books
-      <br />
-      Books
-      <br />
-      Books
-      <br />
-      Books
-      <br />
-      Books
-      <br />
-      Books
-      <br />
-      Books
-      <br />
-      Books
-      <br />
-      Books
-      <br />
+      <Header>
+        <SectionHeading>積み本</SectionHeading>
+      </Header>
+      <Books>
+        <ul>
+          <Book>hoge</Book>
+          <Book>hoge</Book>
+          <Book>hoge</Book>
+          <Book>hoge</Book>
+          <Book>hoge</Book>
+        </ul>
+      </Books>
     </Contents>
   );
 }
