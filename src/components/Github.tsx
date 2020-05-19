0631c1306f2f5b777d57dcf0796c50dbfb4efc49
naoto hamada <hamada@vis-its.com>
2020-05-19 17:20:55 +0900

    Fix typo

--- src/components/Github.tsx
+++ src/components/Github.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import GithubIcon from 'images/github.png';
 
-export default function Twitter() {
+export default function Github() {
   return (
     <div>
       <IconButton
