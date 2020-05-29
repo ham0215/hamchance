9046bb90f4e9f1f424a865f2cf60a04b06bb00ab
naoto hamada <hamada@vis-its.com>
2020-05-30 00:47:00 +0900

    Add design

--- src/pages/Top/Contents.tsx
+++ src/pages/Top/Contents.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import BlackSection from 'components/BlackSection';
 import SectionHeading from 'components/SectionHeading';
-import Link from '@material-ui/core/Link';
 
 const ContentsWrapper = styled.div`
   display: table;
@@ -12,13 +11,57 @@ const ContentsWrapper = styled.div`
 `;
 
 const ContentsBox = styled.div`
+  position: relative;
   display: table-cell;
+
+  ::after {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: 0;
+    width: 100%;
+    height: 50%;
+    background: url('/images/contents/hamchance.png') no-repeat;
+    background-size: 100% 100%;
+    opacity: 0.2;
+    transform: scaleY(-1);
+  }
 `;
 
 const ContentsImage = styled.img`
   display: block;
   width: 100%;
-  max-height: 300px;
+  margin: 0 0 60%;
+`;
+
+const ContentsDescription = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 0;
+  width: 100%;
+  height: 50%;
+`;
+
+const ContentsDescriptionInner = styled.div`
+  display: table;
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+`;
+
+const ContentsText = styled.p`
+  display: table-cell;
+  vertical-align: middle;
+  font-size: 1.2rem;
+  line-height: 2;
+`;
+
+const ContentsButton = styled.a`
+  width: 60%;
+  margin-top: 20px;
+  padding: 3px;
+  border: 1px solid #fff;
+  background-color: rgba(255, 255, 255, 0.15);
 `;
 
 export default function About() {
@@ -31,49 +74,65 @@ export default function About() {
             src="/images/contents/hamchance.png"
             alt="hamchance.com"
           />
-          <p>
-            一つ目の事例
-            <br />
-            説明
-            <br />
-            <Link href="/">READ MORE</Link>
-          </p>
+          <ContentsDescription>
+            <ContentsDescriptionInner>
+              <ContentsText>
+                一つ目の事例
+                <br />
+                説明
+                <br />
+                <ContentsButton href="/">READ MORE</ContentsButton>
+              </ContentsText>
+            </ContentsDescriptionInner>
+          </ContentsDescription>
         </ContentsBox>
         <ContentsBox>
           <ContentsImage src="/images/contents/hamkabu.png" alt="ハム株" />
-          <p>
-            一つ目の事例
-            <br />
-            説明
-            <br />
-            <Link href="/">READ MORE</Link>
-          </p>
+          <ContentsDescription>
+            <ContentsDescriptionInner>
+              <ContentsText>
+                一つ目の事例
+                <br />
+                説明
+                <br />
+                <ContentsButton href="/">READ MORE</ContentsButton>
+              </ContentsText>
+            </ContentsDescriptionInner>
+          </ContentsDescription>
         </ContentsBox>
         <ContentsBox>
           <ContentsImage
             src="/images/contents/tajiri-tozan.png"
             alt="Tajiri Tozan"
           />
-          <p>
-            一つ目の事例
-            <br />
-            説明
-            <br />
-            <Link href="/">READ MORE</Link>
-          </p>
+          <ContentsDescription>
+            <ContentsDescriptionInner>
+              <ContentsText>
+                一つ目の事例
+                <br />
+                説明
+                <br />
+                <ContentsButton href="/">READ MORE</ContentsButton>
+              </ContentsText>
+            </ContentsDescriptionInner>
+          </ContentsDescription>
         </ContentsBox>
         <ContentsBox>
           <ContentsImage
             src="/images/contents/yamanotenki.jpg"
             alt="Tajiri Tozan"
           />
-          <p>
-            一つ目の事例
-            <br />
-            説明
-            <br />
-            <Link href="/">READ MORE</Link>
-          </p>
+          <ContentsDescription>
+            <ContentsDescriptionInner>
+              <ContentsText>
+                一つ目の事例
+                <br />
+                説明
+                <br />
+                <ContentsButton href="/">READ MORE</ContentsButton>
+              </ContentsText>
+            </ContentsDescriptionInner>
+          </ContentsDescription>
         </ContentsBox>
       </ContentsWrapper>
     </BlackSection>
