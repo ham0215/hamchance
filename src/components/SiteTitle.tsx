956968c3d951b745407b71759aba97cb452a4a41
naoto hamada <hamada@vis-its.com>
2020-06-07 12:58:34 +0900

    Fix sp

--- src/components/SiteTitle.tsx
+++ src/components/SiteTitle.tsx
@@ -4,6 +4,10 @@ const SiteTitle = styled.h1`
   margin-top: 35px;
   font-size: 5rem;
   word-wrap: break-word;
+
+  @media (max-width: 768px) {
+    font-size: 3rem;
+  }
 `;
 
 export default SiteTitle;
