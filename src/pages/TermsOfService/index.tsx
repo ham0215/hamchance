4d5cc96851a9715b50447f71f438ff6b9c967a61
naoto hamada <hamada@vis-its.com>
2020-08-11 21:43:00 +0900

    Remove chakra-ui

--- src/pages/TermsOfService/index.tsx
+++ src/pages/TermsOfService/index.tsx
@@ -1,34 +1,32 @@
 import React from 'react';
 import styled from 'styled-components/macro';
-import { List, ListItem, Text } from '@chakra-ui/core';
+import { List, ListItem } from '@f/core';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
-const RightDiv = styled.div`
-  text-align: right;
-  margin: 10px 10px 10px 10px;
-  font-size: medium;
+const Content = styled.div`
+  max-width: 760px;
+  margin: 0 auto 0 auto;
 `;
 
-const Content = styled.div`
-  width: 760px;
-  margin: 0 auto 0 auto;
+const Body = styled(Paper)`
+  padding: 20px 20px 20px 20px;
 `;
 
 export default function TermsOfService() {
   return (
     <Content>
-      <RightDiv>
+      <Typography align="right">
         作成日：2020/04/20
         <br />
         最終更新日：2020/04/20
-      </RightDiv>
-      <Paper>
-        <Text fontSize="4xl">利用規約</Text>
-        <Text fontSize="md">
+      </Typography>
+      <Body>
+        <Typography variant="h4">利用規約</Typography>
+        <Typography>
           この利用規約（以下，「本規約」といいます。）は，hamchance（以下，「当社」といいます。）がこのウェブサイト上で提供するサービス（以下，「本サービス」といいます。）の利用条件を定めるものです。登録ユーザーの皆さま（以下，「ユーザー」といいます。）には，本規約に従って，本サービスをご利用いただきます。
-        </Text>
-        <Text fontSize="3xl">第1条（適用）</Text>
+        </Typography>
+        <Typography variant="h5">第1条（適用）</Typography>
         <List as="ol" styleType="decimal">
           <ListItem>
             本規約は，ユーザーと当社との間の本サービスの利用に関わる一切の関係に適用されるものとします。
@@ -40,7 +38,7 @@ export default function TermsOfService() {
             本規約の規定が前条の個別規定の規定と矛盾する場合には，個別規定において特段の定めなき限り，個別規定の規定が優先されるものとします。
           </ListItem>
         </List>
-        <Text fontSize="3xl">第2条（利用登録）</Text>
+        <Typography variant="h5">第2条（利用登録）</Typography>
         <List as="ol" styleType="decimal">
           <ListItem>
             本サービスにおいては，登録希望者が本規約に同意の上，当社の定める方法によって利用登録を申請し，当社がこの承認を登録希望者に通知することによって，利用登録が完了するものとします。
@@ -54,7 +52,7 @@ export default function TermsOfService() {
             </List>
           </ListItem>
         </List>
-        <Text fontSize="3xl">第3条（ユーザーIDおよびパスワードの管理）</Text>
+        <Typography variant="h5">第3条（ユーザーIDおよびパスワードの管理）</Typography>
         <List as="ol" styleType="decimal">
           <ListItem>
             ユーザーは，自己の責任において，本サービスのユーザーIDおよびパスワードを適切に管理するものとします。
@@ -66,7 +64,7 @@ export default function TermsOfService() {
             ユーザーID及びパスワードが第三者によって使用されたことによって生じた損害は，当社に故意又は重大な過失がある場合を除き，当社は一切の責任を負わないものとします。
           </ListItem>
         </List>
-        <Text fontSize="3xl">第4条（利用料金および支払方法）</Text>
+        <Typography variant="h5">第4条（利用料金および支払方法）</Typography>
         <List as="ol" styleType="decimal">
           <ListItem>
             ユーザーは，本サービスの有料部分の対価として，当社が別途定め，本ウェブサイトに表示する利用料金を，当社が指定する方法により支払うものとします。
@@ -75,7 +73,7 @@ export default function TermsOfService() {
             ユーザーが利用料金の支払を遅滞した場合には，ユーザーは年14．6％の割合による遅延損害金を支払うものとします。
           </ListItem>
         </List>
-        <Text fontSize="3xl">第5条（禁止事項）</Text>
+        <Typography variant="h5">第5条（禁止事項）</Typography>
         <List as="ol" styleType="decimal">
           <ListItem>法令または公序良俗に違反する行為</ListItem>
           <ListItem>犯罪行為に関連する行為</ListItem>
@@ -116,7 +114,7 @@ export default function TermsOfService() {
           <ListItem>宗教活動または宗教団体への勧誘行為</ListItem>
           <ListItem>その他，当社が不適切と判断する行為</ListItem>
         </List>
-        <Text fontSize="3xl">第6条（本サービスの提供の停止等）</Text>
+        <Typography variant="h5">第6条（本サービスの提供の停止等）</Typography>
         <List as="ol" styleType="decimal">
           <ListItem>
             当社は，以下のいずれかの事由があると判断した場合，ユーザーに事前に通知することなく本サービスの全部または一部の提供を停止または中断することができるものとします。
@@ -133,7 +131,7 @@ export default function TermsOfService() {
             当社は，本サービスの提供の停止または中断により，ユーザーまたは第三者が被ったいかなる不利益または損害についても，一切の責任を負わないものとします。
           </ListItem>
         </List>
-        <Text fontSize="3xl">第7条（著作権）</Text>
+        <Typography variant="h5">第7条（著作権）</Typography>
         <List as="ol" styleType="decimal">
           <ListItem>
             ユーザーは，自ら著作権等の必要な知的財産権を有するか，または必要な権利者の許諾を得た文章，画像や映像等の情報に関してのみ，本サービスを利用し，投稿ないしアップロードすることができるものとします。
@@ -145,7 +143,7 @@ export default function TermsOfService() {
             前項本文の定めるものを除き，本サービスおよび本サービスに関連する一切の情報についての著作権およびその他の知的財産権はすべて当社または当社にその利用を許諾した権利者に帰属し，ユーザーは無断で複製，譲渡，貸与，翻訳，改変，転載，公衆送信（送信可能化を含みます。），伝送，配布，出版，営業使用等をしてはならないものとします。
           </ListItem>
         </List>
-        <Text fontSize="3xl">第8条（利用制限および登録抹消）</Text>
+        <Typography variant="h5">第8条（利用制限および登録抹消）</Typography>
         <List as="ol" styleType="decimal">
           <ListItem>
             当社は，ユーザーが以下のいずれかに該当する場合には，事前の通知なく，投稿データを削除し，ユーザーに対して本サービスの全部もしくは一部の利用を制限しまたはユーザーとしての登録を抹消することができるものとします。
@@ -166,9 +164,9 @@ export default function TermsOfService() {
             当社は，本条に基づき当社が行った行為によりユーザーに生じた損害について，一切の責任を負いません。
           </ListItem>
         </List>
-        <Text fontSize="3xl">第9条（退会）</Text>
-        <Text fontSize="md">ユーザーは，当社の定める退会手続により，本サービスから退会できるものとします。</Text>
-        <Text fontSize="3xl">第10条（保証の否認および免責事項）</Text>
+        <Typography variant="h5">第9条（退会）</Typography>
+        <Typography>ユーザーは，当社の定める退会手続により，本サービスから退会できるものとします。</Typography>
+        <Typography variant="h5">第10条（保証の否認および免責事項）</Typography>
         <List as="ol" styleType="decimal">
           <ListItem>
             当社は，本サービスに事実上または法律上の瑕疵（安全性，信頼性，正確性，完全性，有効性，特定の目的への適合性，セキュリティなどに関する欠陥，エラーやバグ，権利侵害などを含みます。）がないことを明示的にも黙示的にも保証しておりません。
@@ -183,37 +181,35 @@ export default function TermsOfService() {
             当社は，本サービスに関して，ユーザーと他のユーザーまたは第三者との間において生じた取引，連絡または紛争等について一切責任を負いません。
           </ListItem>
         </List>
-        <Text fontSize="3xl">第11条（サービス内容の変更等）</Text>
-        <Text fontSize="md">
+        <Typography variant="h5">第11条（サービス内容の変更等）</Typography>
+        <Typography>
           当社は，ユーザーに通知することなく，本サービスの内容を変更しまたは本サービスの提供を中止することができるものとし，これによってユーザーに生じた損害について一切の責任を負いません。
-        </Text>
-        <Text fontSize="3xl">第12条（利用規約の変更）</Text>
-        <Text fontSize="md">
+        </Typography>
+        <Typography variant="h5">第12条（利用規約の変更）</Typography>
+        <Typography>
           当社は，必要と判断した場合には，ユーザーに通知することなくいつでも本規約を変更することができるものとします。なお，本規約の変更後，本サービスの利用を開始した場合には，当該ユーザーは変更後の規約に同意したものとみなします。
-        </Text>
-        <Text fontSize="3xl">第13条（個人情報の取扱い）</Text>
-        <Text fontSize="md">
+        </Typography>
+        <Typography variant="h5">第13条（個人情報の取扱い）</Typography>
+        <Typography>
           当社は，本サービスの利用によって取得する個人情報については，当社「プライバシーポリシー」に従い適切に取り扱うものとします。
-        </Text>
-        <Text fontSize="3xl">第14条（通知または連絡）</Text>
-        <Text fontSize="md">
+        </Typography>
+        <Typography variant="h5">第14条（通知または連絡）</Typography>
+        <Typography>
           ユーザーと当社との間の通知または連絡は，当社の定める方法によって行うものとします。当社は,ユーザーから,当社が別途定める方式に従った変更届け出がない限り,現在登録されている連絡先が有効なものとみなして当該連絡先へ通知または連絡を行い,これらは,発信時にユーザーへ到達したものとみなします。
-        </Text>
-        <Text fontSize="3xl">第15条（権利義務の譲渡の禁止）</Text>
-        <Text fontSize="md">
+        </Typography>
+        <Typography variant="h5">第15条（権利義務の譲渡の禁止）</Typography>
+        <Typography>
           ユーザーは，当社の書面による事前の承諾なく，利用契約上の地位または本規約に基づく権利もしくは義務を第三者に譲渡し，または担保に供することはできません。
-        </Text>
-        <Text fontSize="3xl">第16条（準拠法・裁判管轄）</Text>
+        </Typography>
+        <Typography variant="h5">第16条（準拠法・裁判管轄）</Typography>
         <List as="ol" styleType="decimal">
           <ListItem>本規約の解釈にあたっては，日本法を準拠法とします。</ListItem>
           <ListItem>
             本サービスに関して紛争が生じた場合には，当社の本店所在地を管轄する裁判所を専属的合意管轄とします。
           </ListItem>
         </List>
-        <Text textAlign="right" fontSize="md">
-          以上
-        </Text>
-      </Paper>
+        <Typography align="right">以上</Typography>
+      </Body>
     </Content>
   );
 }
