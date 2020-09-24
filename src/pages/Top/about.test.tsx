221e4bb12e4cfd20f33ef547796bc397a7f8c3d6
naoto hamada <hamada@vis-its.com>
2020-09-25 00:05:51 +0900

    Feature/update package (#45)

--- src/pages/Top/about.test.tsx
+++ src/pages/Top/about.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
 import { act } from 'react-dom/test-utils';
 
-import About from '..';
+import About from '.';
 
 let container: HTMLDivElement | null = null;
 
