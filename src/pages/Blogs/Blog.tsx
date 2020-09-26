8a4269e88134e266f12f238c609de3e6c9929554
naoto hamada <hamada@vis-its.com>
2020-09-26 23:40:30 +0900

    Fix design

--- src/pages/Blogs/Blog.tsx
+++ src/pages/Blogs/Blog.tsx
@@ -1,5 +1,19 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import ReactMarkdown from 'react-markdown';
+import { Link } from 'react-router-dom';
+import MLink from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+
+const BackLink = styled.div`
+  margin: 10px;
+  font-size: 0.8rem;
+`;
+
+const BlogArea = styled(Paper)`
+  margin: 10px;
+  padding: 20px;
+`;
 
 type Props = {
   mdPath: string;
@@ -14,5 +28,16 @@ export default ({ mdPath }: Props) => {
       setMarkdown(text);
     });
 
-  return <ReactMarkdown source={markdown} escapeHtml={false} />;
+  return (
+    <div>
+      <BackLink>
+        <MLink component={Link} to="/blogs" color="inherit" underline="none">
+          ＜ 一覧へ戻る
+        </MLink>
+      </BackLink>
+      <BlogArea>
+        <ReactMarkdown source={markdown} escapeHtml={false} />
+      </BlogArea>
+    </div>
+  );
 };
--- src/pages/Blogs/index.tsx
+++ src/pages/Blogs/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch, Link } from 'react-router-dom';
+import { List, ListItem, ListItemText } from '@material-ui/core';
 import Blog from './Blog';
 import mds from './data.json';
 
@@ -10,14 +11,18 @@ export default () => {
     </Route>
   ));
   const index = mds.map((md) => (
-    <div>
-      <Link to={`/blogs/${md}`}>{md}</Link>
-    </div>
+    <ListItem button component={Link} to={`/blogs/${md}`}>
+      <ListItemText primary={md} />
+    </ListItem>
   ));
   return (
     <Switch>
       {blogs}
-      <Route>{index}</Route>
+      <Route>
+        <List component="nav" aria-label="secondary mailbox folders">
+          {index}
+        </List>
+      </Route>
     </Switch>
   );
 };
