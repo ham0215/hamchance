358d32e7137c6407c7fb59b61156caa8879ad2a3
naoto hamada <hamada@vis-its.com>
2020-11-12 00:19:23 +0900

    Disable books (#52)

--- src/pages/Books/Top/index.tsx
+++ src/pages/Books/Top/index.tsx
@@ -1,15 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+// import React, { useState, useEffect } from 'react';
 import styled from 'styled-components/macro';
-import Grid from '@material-ui/core/Grid';
+// import Grid from '@material-ui/core/Grid';
 import SectionHeading from 'components/SectionHeading';
 import firebase from 'components/Firebase';
 import BlackSection from 'components/BlackSection';
-import Book from './Book';
+// import Book from './Book';
 
-const Hondana = styled(Grid)`
-  background: #151515 url('/images/hondana.png') repeat;
-  background-size: 230px, auto;
-`;
+// const Hondana = styled(Grid)`
+//   background: #151515 url('/images/hondana.png') repeat;
+//   background-size: 230px, auto;
+// `;
 
 const Header = styled.div`
   height: 150px;
@@ -27,43 +28,43 @@ type Tsumihon = {
 };
 
 export default function Top() {
-  const db = firebase.firestore();
-  const [tsumihon, setTsumihon] = useState<Tsumihon>({
-    books: {},
-    bookOrder: [],
-  });
+  // const db = firebase.firestore();
+  // const [tsumihon, setTsumihon] = useState<Tsumihon>({
+  //   books: {},
+  //   bookOrder: [],
+  // });
 
-  useEffect(() => {
-    (async () => {
-      const queryBooks = await db.collection('books').get();
-      const queryOrder = await db.collection('orders').doc('bookOrder').get();
-      const bs: Books = {};
-      queryBooks.forEach((doc) => {
-        bs[doc.id] = doc.data();
-      });
-      setTsumihon({
-        books: bs,
-        bookOrder: queryOrder.data()?.bookOrder,
-      });
-    })();
-  }, [db, setTsumihon]);
+  // useEffect(() => {
+  //   (async () => {
+  //     const queryBooks = await db.collection('books').get();
+  //     const queryOrder = await db.collection('orders').doc('bookOrder').get();
+  //     const bs: Books = {};
+  //     queryBooks.forEach((doc) => {
+  //       bs[doc.id] = doc.data();
+  //     });
+  //     setTsumihon({
+  //       books: bs,
+  //       bookOrder: queryOrder.data()?.bookOrder,
+  //     });
+  //   })();
+  // }, [db, setTsumihon]);
 
-  let books = [];
-  if (tsumihon.bookOrder) {
-    books = tsumihon.bookOrder.map((key: string) => {
-      const { title, url, imgSrc, imgTagSrc } = tsumihon.books[key];
-      return <Book key={key} title={title} url={url} imgSrc={imgSrc} imgTagSrc={imgTagSrc} />;
-    });
-  }
+  // let books = [];
+  // if (tsumihon.bookOrder) {
+  //   books = tsumihon.bookOrder.map((key: string) => {
+  //     const { title, url, imgSrc, imgTagSrc } = tsumihon.books[key];
+  //     return <Book key={key} title={title} url={url} imgSrc={imgSrc} imgTagSrc={imgTagSrc} />;
+  //   });
+  // }
 
   return (
     <BlackSection>
       <Header>
         <SectionHeading>積み本</SectionHeading>
       </Header>
-      <Hondana container justify="center">
+      {/* <Hondana container justify="center">
         {books}
-      </Hondana>
+      </Hondana> */}
     </BlackSection>
   );
 }
