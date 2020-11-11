166371b3c2707d151fa4e12d59d79a7acd77f292
naoto hamada <hamada@vis-its.com>
2020-11-12 01:33:31 +0900

    WIP

---
+++ src/components/InnerContents.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components/macro';
+
+const InnerContents = styled.div`
+  margin: 20px 0 20px 0;
+`;
+
+export default InnerContents;
--- src/pages/AdventCalendar/index.tsx
+++ src/pages/AdventCalendar/index.tsx
@@ -1,28 +1,26 @@
 import React from 'react';
-import { Route, Switch, Link } from 'react-router-dom';
-import { List, ListItem, ListItemText } from '@material-ui/core';
+import { List, ListItem, ListItemText, Typography } from '@material-ui/core';
+import Contents from 'components/Contents';
+import InnerContents from 'components/InnerContents';
 import Blog from './Blog';
-import mds from './data.json';
 
 export default () => {
-  const blogs = mds.map((md) => (
-    <Route key={md} path={`/adventcalendar/${md}`}>
-      <Blog mdPath={`/mds/${md}.md`} />
-    </Route>
-  ));
-  const index = mds.map((md) => (
-    <ListItem button component={Link} to={`/adventcalendar/${md}`}>
-      <ListItemText primary={md} />
-    </ListItem>
-  ));
+  // const blogs = mds.map((md) => (
+  //   <Route key={md} path={`/adventcalendar/${md}`}>
+  //     <Blog mdPath={`/mds/${md}.md`} />
+  //   </Route>
+  // ));
+  // const index = mds.map((md) => (
+  //   <ListItem button component={Link} to={`/adventcalendar/${md}`}>
+  //     <ListItemText primary={md} />
+  //   </ListItem>
+  // ));
   return (
-    <Switch>
-      {blogs}
-      <Route>
-        <List component="nav" aria-label="secondary mailbox folders">
-          {index}
-        </List>
-      </Route>
-    </Switch>
+    <Contents>
+      <Typography variant="h4">Advent Calendar 2020</Typography>
+      <InnerContents>
+        <Typography variant="subtitle1">システム開発のTipsが集まったAdvent Calendarです。</Typography>
+      </InnerContents>
+    </Contents>
   );
 };
