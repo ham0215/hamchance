e00d4164e8327d7bc1003feacb89d4663f89d379
naoto hamada <hamada@vis-its.com>
2020-12-08 01:19:01 +0900

    Add item

--- src/pages/DevTips/Items.tsx
+++ src/pages/DevTips/Items.tsx
@@ -26,5 +26,47 @@ export default () => (
       icon={<FastfoodIcon />}
     />
     <Item title="判定に複数条件を使う場合はメソッドを作る" to="/devtips" linkType="internal" icon={<FastfoodIcon />} />
+    <Item title="同じ判定条件でも意味が違うならaliasを作る" to="/devtips" linkType="internal" icon={<FastfoodIcon />} />
+    <Item title="その定数、本当に必要ですか？" to="/devtips" linkType="internal" icon={<FastfoodIcon />} />
+    <Item title="定数名に値の情報を入れすぎない" to="/devtips" linkType="internal" icon={<FastfoodIcon />} />
+    <Item
+      title="三項演算子やカッコなしのifはやめよう。は嘘"
+      to="/devtips"
+      linkType="internal"
+      icon={<FastfoodIcon />}
+    />
+    <Item title="複雑な処理の入り口は1つにしよう" to="/devtips" linkType="internal" icon={<FastfoodIcon />} />
+    <Item title="早期リターンしよう" to="/devtips" linkType="internal" icon={<FastfoodIcon />} />
+    <Item title="if条件は組み合わせずに分割しよう" to="/devtips" linkType="internal" icon={<FastfoodIcon />} />
+    <Item
+      title="汎用的な機能を実装するときは使う側が簡単に使えるか？を意識しよう"
+      to="https://qiita.com/ham0215/items/b3938c5ee6fab08fceaa"
+      linkType="external"
+      icon={<FastfoodIcon />}
+    />
+    <Item title="勘違いしない名前をつけよう" to="/devtips" linkType="internal" icon={<FastfoodIcon />} />
+    <Item title="見てわかるコメント入れない" to="/devtips" linkType="internal" icon={<FastfoodIcon />} />
+    <Item
+      title="レビューで理解しづらいコードを見かけたら指摘しよう"
+      to="/devtips"
+      linkType="internal"
+      icon={<FastfoodIcon />}
+    />
+    <Item
+      title="Webアプリケーションのコードレビューで気にしている観点"
+      to="https://qiita.com/ham0215/items/493d31b26497e4a671a4"
+      linkType="external"
+      icon={<FastfoodIcon />}
+    />
+    <Item
+      section="Gitの章"
+      title="Gitユースケース "
+      to="https://qiita.com/ham0215/items/b4c68f1c63e2fa710326"
+      linkType="external"
+      icon={<FastfoodIcon />}
+    />
+    <Item title="作業するプルリクは1人ごとに作る" to="/devtips" linkType="internal" icon={<FastfoodIcon />} />
+    <Item title="squashマージの罠" to="/devtips" linkType="internal" icon={<FastfoodIcon />} />
+    <Item title="gitでマージの流れを守ろう" to="/devtips" linkType="internal" icon={<FastfoodIcon />} />
   </Timeline>
 );
