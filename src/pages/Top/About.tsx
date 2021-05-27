28edb49d84c79b4d57174fbdbf847be374db5ba8
naoto hamada <hamada@vis-its.com>
2021-05-27 10:21:52 +0900

    Fix about text

--- src/pages/Top/About.tsx
+++ src/pages/Top/About.tsx
@@ -25,24 +25,10 @@ export default function About() {
         が趣味の合間に気になる技術を試すためのページです。
       </SectionText>
       <SectionSubText>
-        このサイトの各機能に特に意味はないのでご注意ください。
-        <br />
-        例えばヘッダーにログインボタンがありますがログインしても何もできませんw
-        (firebaseのログインを試したかっただけです)
-        <br />
         雑に試すため、機能が急に増減したり、表示が崩れたり、Internal Server
         Errorで何もできなくなったりするかもしれません。
       </SectionSubText>
       <SectionSubText>
-        デザインスキルやCSSはめっぽう弱いのでトップページの構成は『
-        <Link
-          href="https://www.amazon.co.jp/gp/product/4798141577/ref=as_li_tl?ie=UTF8&camp=247&creative=1211&creativeASIN=4798141577&linkCode=as2&tag=hamchance0215-22&linkId=6a6e4551d2657be755bde09ab7b7f14e"
-          target="_blank"
-        >
-          HTML5/CSS3モダンコーディング
-        </Link>
-        』をベースに<Link href="https://ja.reactjs.org/">React</Link>
-        で作成しました。
         <Topics>
           <Typography variant="subtitle1" component="span">
             Keywords of the technology used on this site
