7958b8a95b4a3ec06fd9f1f6c1d4bd226242e8b6
naoto hamada <hamada@vis-its.com>
2021-09-24 15:38:14 +0900

    Fix eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,8 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import { useState } from 'react';
 
 const Home: NextPage = () => {
-  let hoge = '';
-  let fuga = hoge;
-
   return (
     <div className={styles.container}>
       <Head>
