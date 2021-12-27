fe2884ae38be092a19d839c105f129318ab07bb5
naoto hamada <hamada@vis-its.com>
2021-12-27 16:47:54 +0900

    Add snapshot test (#145)
    * Add snapshot test
    
    * Fix prettier

---
+++ src/components/PageLayout/index.test.tsx
@@ -0,0 +1,13 @@
+import PageLayout from '.';
+import { render } from '@testing-library/react';
+
+describe('Home', () => {
+  it('snapshot testing', () => {
+    const { asFragment } = render(
+      <PageLayout>
+        <div>hoge</div>
+      </PageLayout>
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
