dc2dc50e12757dbd36e86600f7e5ab06dc2fee8c
naoto hamada <hamada@vis-its.com>
2021-12-27 10:32:19 +0900

    Fix test

--- src/components/Home/index.test.tsx
+++ src/components/Home/index.test.tsx
@@ -1,11 +1,11 @@
-import Home from '../../domains/Home';
+import Home from '.';
 import { render, screen } from '@testing-library/react';
 
 describe('Home', () => {
   it('renders a heading', async () => {
     render(<Home />);
     screen.getByRole('heading', {
-      name: /welcome to next\.js!/i,
+      name: /Hamchance\.com/i,
     });
   });
 });
