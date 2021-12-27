175a0c9d335add537c79c2bd24594a6f7aea4472
naoto hamada <hamada@vis-its.com>
2021-12-27 17:38:34 +0900

    Install storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,35 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true,
+    es6: true
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:jest/recommended',
-    'prettier',
-    'next/core-web-vitals'
-  ],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:jest/recommended", "prettier", "next/core-web-vitals", "plugin:storybook/recommended"],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
     warnOnUnsupportedTypeScriptVersion: true,
     ecmaFeatures: {
-      jsx: true,
-    },
+      jsx: true
+    }
   },
   plugins: ['@typescript-eslint', 'jest'],
   settings: {
     react: {
-      version: 'detect',
-    },
+      version: 'detect'
+    }
   },
   rules: {
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
     'eol-last': ['error', 'always'],
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
+    '@typescript-eslint/no-unused-vars': ['error', {
+      ignoreRestSiblings: true
+    }],
     'jest/expect-expect': 'off',
     'jest/require-top-level-describe': 'error',
     'jest/no-disabled-tests': 'off',
-    'no-console': 'error',
-  },
+    'no-console': 'error'
+  }
 };
