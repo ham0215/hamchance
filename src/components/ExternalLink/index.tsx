5874452e40df300a4269baf21ea7c0393eee95ca
naoto hamada <hamada@vis-its.com>
2021-12-28 17:37:29 +0900

    Fix external link

--- src/components/ExternalLink/index.tsx
+++ src/components/ExternalLink/index.tsx
@@ -1,15 +1,12 @@
 import { ReactNode } from 'react';
-import NextLink from 'next/link';
 import Link from '@mui/material/Link';
 
 type Props = { href: string; prefetch?: boolean; children: ReactNode };
 
-export default function ExternalLink({ href, prefetch = false, children }: Props) {
+export default function ExternalLink({ href, children }: Props) {
   return (
-    <NextLink href={href} prefetch={prefetch}>
-      <Link href={href} color="inherit" underline="none" target="_blank" rel="noopener noreferrer">
-        {children}
-      </Link>
-    </NextLink>
+    <Link href={href} color="inherit" underline="none" target="_blank" rel="noopener noreferrer">
+      {children}
+    </Link>
   );
 }
