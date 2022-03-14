5c2fecacf07ca6c9ffa318acf0c17301410faeeb
naoto hamada <hamada@vis-its.com>
2022-03-14 16:56:48 +0900

    Add staticDirs

--- .storybook/main.js
+++ .storybook/main.js
@@ -14,5 +14,6 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials"
   ],
-  framework: "@storybook/react"
+  framework: "@storybook/react",
+  staticDirs: ['../public'],
 }
