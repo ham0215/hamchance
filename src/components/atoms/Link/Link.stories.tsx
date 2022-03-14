295893fe3ed46e9cdfe450d138268b3c2ba6ffee
naoto hamada <hamada@vis-its.com>
2022-03-14 16:11:03 +0900

    Add ExternalLink strorybook

--- src/components/atoms/Link/Link.stories.tsx
+++ src/components/atoms/Link/Link.stories.tsx
@@ -7,14 +7,11 @@ export default {
   component: Link,
 } as ComponentMeta<typeof Link>;
 
-const Template: ComponentStory<typeof Link> = (args) => (
-  <Link {...args}>
-    Link Children
-  </Link>
-);
+const Template: ComponentStory<typeof Link> = (args) => <Link {...args}>Link</Link>;
 
 export const Default = Template.bind({});
 
 Default.args = {
-  href: 'http://localhost:3000'
+  href: 'http://localhost:3000',
+  prefetch: false,
 };
