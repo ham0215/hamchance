2bcce7e28699bb5d11205cff23e07fc795f0fad7
naoto hamada <hamada@vis-its.com>
2022-03-15 08:59:12 +0900

    Add staticDirs (#199)
    * Add staticDirs
    
    * Fix next/image
    
    * Fix

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,21 @@
 import ThemeProvider from 'providers/ThemeProvider';
 import PaletteModeProvider from 'providers/PaletteModeProvider';
+import * as nextImage from 'next/image';
+
+Object.defineProperty(nextImage, 'default', {
+  configurable: true,
+  value: props => <img {...props} />
+});
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
 
 export const decorators = [
   (Story) => (
