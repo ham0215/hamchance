4e68c066532b59409d133802067a3df4cc05de20
naoto hamada <hamada@vis-its.com>
2022-03-14 17:53:42 +0900

    Fix next/image

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,23 @@
 import ThemeProvider from 'providers/ThemeProvider';
 import PaletteModeProvider from 'providers/PaletteModeProvider';
+import * as nextImage from 'next/image';
+
+import { withThemes } from "@react-theming/storybook-addon"
+
+Object.defineProperty(nextImage, 'default', {
+  configurable: true,
+  value: props => <img {...props} />
+});
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
 
 export const decorators = [
   (Story) => (
