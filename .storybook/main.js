995218827a2d978a13f1c9cdc33ab3c4b30b0212
naoto hamada <hamada@vis-its.com>
2022-03-14 16:20:46 +0900

    Add Header strorybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,18 @@
+const path = require("path");
+
 module.exports = {
-  "stories": [
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.modules.push(path.resolve(__dirname, '../src'));
+
+    return config;
+  },
+  stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
   ],
-  "framework": "@storybook/react"
+  framework: "@storybook/react"
 }
