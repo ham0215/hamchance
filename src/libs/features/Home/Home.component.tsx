e3750ccc5ee837250544e481040f9717be9fdc84
Naoto Hamada <naoto.hamada@findy.co.jp>
2023-10-31 09:07:44 +0900

    Fix size

--- src/libs/features/Home/Home.component.tsx
+++ src/libs/features/Home/Home.component.tsx
@@ -24,7 +24,7 @@ export function Home() {
             title="Naoto Hamada"
             subheader="Software Engineer"
           />
-          <CardContent sx={{ p: 4 }}>
+          <CardContent sx={{ p: 2 }}>
             <Grid container spacing={2}>
               <ProductLink
                 href="https://twitter.com/hamchance0215"
@@ -32,40 +32,40 @@ export function Home() {
                 name="@hamchance0215"
                 onClickQr={handleOpenQrModal}
               >
-                <SiX color="black" size={50} />
+                <SiX color="black" size={46} />
               </ProductLink>
               <ProductLink href="https://github.com/ham0215" productName="GitHub" name="ham0215">
-                <SiGithub color="black" size={50} />
+                <SiGithub color="black" size={46} />
               </ProductLink>
               <ProductLink href="https://qiita.com/ham0215" productName="Qiita" name="@ham0215">
-                <SiQiita color="#55C500" size={50} />
+                <SiQiita color="#55C460" size={46} />
               </ProductLink>
               <ProductLink href="https://speakerdeck.com/ham0215" productName="Speaker Deck" name="ham0215">
-                <SiSpeakerdeck color="#009287" size={50} />
+                <SiSpeakerdeck color="#009287" size={46} />
               </ProductLink>
               <ProductLink href="https://zenn.dev/hamchance" productName="Zenn" name="@hamchance">
-                <SiZenn color="#3EA8FF" size={50} />
+                <SiZenn color="#3EA8FF" size={46} />
               </ProductLink>
               <ProductLink href="https://note.com/hamchance" productName="Note" name="@hamchance">
-                <Image src="/static/images/note2.svg" alt="note" width="50" height="50" />
+                <Image src="/static/images/note2.svg" alt="note" width="46" height="46" />
               </ProductLink>
               <ProductLink href="https://www.linkedin.com/in/ham0215/" productName="LinkedIn" name="ham0215">
-                <SiLinkedin color="#2867B2" size={50} />
+                <SiLinkedin color="#2867B2" size={46} />
               </ProductLink>
               <ProductLink href="https://www.wantedly.com/id/hamchance" productName="Wantedly" name="hamchance">
-                <Image src="/static/images/wantedly.svg" alt="wantedly" width="50" height="50" />
+                <Image src="/static/images/wantedly.svg" alt="wantedly" width="46" height="46" />
               </ProductLink>
               <ProductLink href="https://www.facebook.com/naoto.hamada.9" productName="Facebook" name="naoto.hamada">
-                <SiFacebook color="#3b5998" size={50} />
+                <SiFacebook color="#3b5998" size={46} />
               </ProductLink>
               <ProductLink href="https://youtrust.jp/users/ham" productName="YOUTRUST" name="ham">
-                <Image src="/static/images/youtrust.avif" alt="youtrust" width="50" height="50" />
+                <Image src="/static/images/youtrust.avif" alt="youtrust" width="46" height="46" />
               </ProductLink>
               <ProductLink href="https://connpass.com/user/ham0215/" productName="connpass" name="ham0215">
-                <Image src="/static/images/connpass.png" alt="connpass" width="50" height="50" />
+                <Image src="/static/images/connpass.png" alt="connpass" width="46" height="46" />
               </ProductLink>
               <ProductLink href="https://lapras.com/public/ham" productName="LAPRAS" name="ham">
-                <Image src="/static/images/lapras.png" alt="lapras" width="50" height="50" />
+                <Image src="/static/images/lapras.png" alt="lapras" width="46" height="46" />
               </ProductLink>
             </Grid>
           </CardContent>
